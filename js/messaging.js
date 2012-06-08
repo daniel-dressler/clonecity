e6217c0d862db6feabcb096ae3b9bcaf60b402e8
danieru <danieru.dressler@gmail.com>
2012-06-07 20:13:14 -0600

    Messages now fade out after 6 seconds.

--- js/messaging.js
+++ js/messaging.js
@@ -4,7 +4,7 @@ function sayMessage(building, message, lifespan) {
 	var loc = getBldLoc(building);
 	var cord = cordFromLoc(loc);
 	
-	var html = '<div class="message" \
+	var html = '<div class="message lifespan'+lifespan+'" \
 	style="z-index: '+getBldZ(building) + 1 +'; \
 	top: '+cord.y+'px; left: '+cord.x+'px;" >\
 	'+message+'\
