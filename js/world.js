c37f1a3a647755cf130a325f3203d0f2b142b758
danieru <danieru.dressler@gmail.com>
2012-07-01 19:04:16 -0600

    removing most perlin terrain

--- js/world.js
+++ js/world.js
@@ -2,9 +2,12 @@
 
 function generateWorld(width, height)
 {
+	var t = '';
 	for(var posX = 0; posX <= width; posX += TILESIZE) {
 		for(var posY = -0; posY <= height; posY += TILESIZE) {
-				Crafty.e("2D, Canvas, "+mapPosToTile(posX, posY)).attr({x: posX, y: posY});
+				t = mapPosToTile(posX, posY);
+				if (!!t)
+					Crafty.e("2D, Canvas, "+t).attr({x: posX, y: posY});
 		}
 		
 	}
@@ -14,83 +17,62 @@ function mapPosToTile(x, y)
 {
 	x = x / 16;
 	y = y / 16 ;
-	var top_left =  perlin(x, y) + BIAS;
-	var top_right = perlin(x + 1, y) + BIAS;
-	var bot_left =  perlin(x, y + 1) + BIAS;
-	var bot_right = perlin(x + 1, y + 1) + BIAS;
+	var num = perlin(x, y) % 2;
+	if (num < -0.9)
+		return "dirt_tuff";
+	if (num < -0.8)
+		return "dirt_speck";
+	if (num >  0.99)
+		return "dirt_center_3week";
+	if (num >  0.7)
+		return "dirt_center_2week";
+	/*if (num >    0.5)
+		return "dirt_center_1week";
 	
-	var t = "";/* t for tile */
-	/* This is a hardcoded trie tree */
-	if (top_left < 0) {
-		if (top_right < 0) {
-			if (bot_left < 0) {
-				if (bot_right < 0) {
-					t = "dirt_center_empty";
-				} else {
-					t = "grass_corner_outside_top_left";
-				}
-			} else {
-				if (bot_right < 0) {
-					t = "grass_corner_outside_top_right";
-				} else {
-					t = "grass_corner_outside_top_center";
-				}
-			}
-		} else {
-			if (bot_left < 0) {
-				if (bot_right < 0) {
-					t = "grass_corner_outside_bottom_left";
-				} else {
-					t = "grass_corner_outside_center_left";
-				}
-			} else {
-				if (bot_right < 0) {
-					t = "grass_tuff";
-				} else {
-					t = "grass_corner_inside_bottom_right";
-				}
-			}
-		}
-	} else {
-		if (top_right < 0) {
-			if (bot_left < 0) {
-				if (bot_right < 0) {
-					t = "grass_corner_outside_bottom_right";
-				} else {
-					t = "grass_speck";
-				}
-			} else {
-				if (bot_right < 0) {
-					t = "grass_corner_outside_center_right";
-				} else {
-					t = "grass_corner_inside_bottom_left";
-				}
-			}
-		} else {
-			if (bot_left < 0) {
-				if (bot_right < 0) {
-					t = "grass_corner_outside_bottom_center";
-				} else {
-					t = "grass_corner_inside_top_right";
-				}
-			} else {
-				if (bot_right < 0) {
-					t = "grass_corner_inside_top_left";
-				} else {
-					/* Add varity */
-					t = "grass_center_empty";
-				}
-			}
-		}
-	}
 	
-	return t;
-}
+	/*
+	var top_left = Number(perlin(x-1, y-1) > 0);//is grass if > 0?
+	var top_center = Number(perlin(x,   y-1) > 0);
+	var top_right = Number(perlin(x+1, y-1) > 0);
+	
+	var center_left = Number(perlin(x-1,   y) > 0);
+	var center_center = Number(perlin(x,     y) > 0);
+	var center_right = Number(perlin(x+1,   y) > 0);
+	
+	var bottom_left = Number(perlin(x-1, y+1) > 0);
+	var bottom_center = Number(perlin(x,   y+1) > 0);
+	var bottom_right = Number(perlin(x+1, y+1) > 0);
+	
+	if (!center_center)
+		return "dirt_center_empty";
 
+   /* cord[-1][-1] + cord[ 0][-1] + cord[+1][-1]
+    * cord[-1][ 0]                + cord[+1][ 0]
+    * cord[-1][+1] + cord[ 0][+1] + cord[+1][+1]
+	*/
+
+	//smoothing
+	/*
+	if (!top_left && !top_right)
+		top_center = 0;
+	if (!bottom_right && !top_right)
+		center_right = 0;
+	if (!bottom_right && !bottom_left)
+		bottom_center = 0;
+	if (!bottom_left && !top_left)
+		center_left = 0;
+	*//*
+	
+	var t = "" + top_center + center_right + bottom_center + center_left;
+	
+	//console.log(t);
+	return TILEMAP[t];
+	*/
+}
 
 var BIAS = 0.0;
 var TILEMAPPERLIN = new SimplexNoise();
-var persistence = 0.05;
+var persistence = 0.15;
 var frequency = 0.07;
 var amplitude  = 0.6;
 var octaves = 1;
@@ -109,5 +91,5 @@ function perlin(x, y)
         freq *= 2;
     }
 
-    return t  + BIAS*_amplitude;
+    return t;
 }
